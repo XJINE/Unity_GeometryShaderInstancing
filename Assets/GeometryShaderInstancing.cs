f00a772647abb6e0fff37cc0dee540aacd954c49
XJINE <xjine.andorelseif@gmail.com>
2021-01-29 17:49:37 +0900

    Remove unused codes.

--- Assets/GeometryShaderInstancing.cs
+++ Assets/GeometryShaderInstancing.cs
@@ -1,34 +1,14 @@
-﻿using System.Runtime.InteropServices;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GeometryShaderInstancing : MonoBehaviour
 {
     [Range(0, 1000000)]
-    public  int           numOfInstancing;
-    public  Material      material;
-    private ComputeBuffer vertexBuffer;
-
-    void Start()
-    {
-        Vector3[] vertices = new Vector3[]
-        {
-            new Vector3(0, 0, 0)
-        };
-
-        vertexBuffer = new ComputeBuffer(1, Marshal.SizeOf(typeof(Vector3)));
-        vertexBuffer.SetData(vertices);
-
-        material.SetBuffer("_VertexBuffer", vertexBuffer);
-    }
+    public  int      numOfInstancing;
+    public  Material material;
 
     void OnRenderObject()
     {
         material.SetPass(0);
         Graphics.DrawProceduralNow(MeshTopology.Points, numOfInstancing);
-    }
-
-    void OnDestroy()
-    {
-        vertexBuffer.Release();
     }
 }
